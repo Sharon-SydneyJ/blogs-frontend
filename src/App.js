d9acf7810e04153da7c5222cec3af856f92af840
sydney <sydneychase5841@gmail.com>
2021-10-03 16:11:52 -0400

    installed react router dom, created home, navbar components, added home, navbar routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Components/Navbar';
 import irlogo from './irlogo.png';
+import Home from './Components/Home';
+
 import './App.css';
 
 function App() {
   return (
-  
+    <Router>
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <header className="App-header">
         <h1> The Indigo Room Blogs </h1>
         <img src={irlogo} className="App-logo" alt="logo" />
-          
+        <Switch>
+     <Route exact path="/">
+       <Home />
+       </Route>
+       </Switch>
       </header> 
     </div>
-   
+   </Router>
   );
 }
 export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <div className="container-sm">
+        <nav className="navbar d-flex justify-content-center fw-bolder">
+            <div className="links">
+                <Link to="/">Home</Link><br/>
+                {/* <Link to="/UserContainer">Bloggers</Link><br/>
+                <Link to="/BlogpostContainer">Blogs</Link><br/> */}
+                
+            </div>
+        </nav>
+        </div>
+    );
+}
+
+export default Navbar;
