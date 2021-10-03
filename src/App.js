01eff474bb8049881076b49d400a0a52d968e715
sydney <sydneychase5841@gmail.com>
2021-10-03 11:35:43 -0400

    first commit, App.js, index.js, index.html, testing static data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import irlogo from './irlogo.png';
 import './App.css';
 
 function App() {
   return (
+  
     <div className="App">
+      {/* <Navbar /> */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1> The Indigo Room Blogs </h1>
+        <img src={irlogo} className="App-logo" alt="logo" />
+          
+      </header> 
     </div>
+   
   );
 }
-
 export default App;
