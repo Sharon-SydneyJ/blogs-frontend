8448303bd482703136c7ece9bdc0ec82aaf69113
sydney <sydneychase5841@gmail.com>
2021-10-05 21:48:16 -0400

    created and added user/details, container blogpost/details, container added navbar, homepage

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import irlogo from './irlogo.png';
 import Home from './components/Home';
+import UserContainer from './components/User/UserContainer';
+import UserDetails from './components/User/UserDetails';
+import BlogpostContainer from './components/Blog/BlogpostContainer';
+import BlogpostDetails from './components/Blog/BlogpostDetails';
 
 import './App.css';
+
 
 function App() {
   return (
@@ -12,11 +17,22 @@ function App() {
     <div className="App">
       <Navbar />
       <header className="App-header">
-        <h1> The Indigo Room Blogs </h1>
         <img src={irlogo} className="App-logo" alt="logo" />
         <Switch>
      <Route exact path="/">
        <Home />
+       </Route>
+       <Route exact path="/users">
+       <UserContainer />
+       </Route>
+       <Route exact path="/users/:id">
+       <UserDetails />
+       </Route>
+       <Route exact path="/blogposts">
+       <BlogpostContainer />
+       </Route>
+       <Route path="/blogposts/:id">
+       <BlogpostDetails />
        </Route>
        </Switch>
       </header> 
