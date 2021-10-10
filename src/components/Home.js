3e814efdec8cc81c15dd957ef388fd667307f4da
sydney <sydneychase5841@gmail.com>
2021-10-10 02:44:13 -0400

    no changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from 'react'
+
 
 
 function Home() {
 
     
+          
     
-   
-
-  
-
 
     return (
-        <div className="home-container">
-        <h1 className="home-header">Welcome to The Indigo Room Blogs</h1>
+        <div className="box">
         
+        <h1 className="header">Welcome to The Indigo Room Blogs</h1>
+        <div>
+            
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+          </div>
         
         </div>
 
