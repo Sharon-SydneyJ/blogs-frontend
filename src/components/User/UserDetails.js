bc002a3c058fe4e81695ef5b65a8f74d68583897
sydney <sydneychase5841@gmail.com>
2021-10-10 03:09:20 -0400

    changed state from null, added create blogpost  function and post request for blogpost by user_id

--- src/components/User/UserDetails.js
+++ src/components/User/UserDetails.js
@@ -2,11 +2,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { BASE_URL } from "../../constraints/index.js";
+import BlogpostForm from "../Blog/BlogpostForm.js";
 
 
 
 export default function UserDetails() {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState("");
   
     const { id } = useParams();
   
@@ -19,22 +20,46 @@ export default function UserDetails() {
     useEffect(() => {
         console.log(user);
     }, [user]);
+
+    function createBlogPost(blogpostDetails) {
+        const newBlogpost = {
+          ...blogpostDetails,
+          user_id: id,
+        };
+    
+        fetch(BASE_URL + `users/${id}/blogposts`, {
+          method: "POST",
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(newBlogpost),
+        })
+          .then((res) => res.json())
+          .then((json) => {
+            const newUser = { ...user.id, blogpost: [...user.blogposts, json] };
+            setUser(newUser);
+          });
+      }
     
   
     return (
-      <div>
-          {user && (
+        <div>
+        {user && (
           <>
-          <p> {user.first_name} </p>
-          <p>{user.user_name} </p>
-          {/* <p>{user.blogposts}</p> */}
-          
+            <p>First Name: {user.first_name}</p>
+            <p>User Name: {user.user_name}</p>
+            
+            <h3>Blogs</h3>
+            
+            <div className="card-container  ">
+             
+            <h3>Add New Blog post</h3>
+            <BlogpostForm createBlogpost={createBlogPost} />
+            
+        
+            </div>
+            
           </>
-      )}
+        )}
       </div>
-      
-      );
+    );
   
   }
-  
-  
