c78cbc8659486d32a81cca3500f2f2ec184c370e
sydney <sydneychase5841@gmail.com>
2021-10-14 21:27:21 -0400

    added welcome for specific blogger links for blog ideas and text area for blogging, cleaned up blogging form

--- src/components/User/UserDetails.js
+++ src/components/User/UserDetails.js
@@ -2,19 +2,20 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { BASE_URL } from "../../constraints/index.js";
-import BlogpostForm from "../Blog/BlogpostForm.js";
+import BlogpostForm from "../Blog/BlogpostForm.js"
 
 
 
 export default function UserDetails() {
     const [user, setUser] = useState("");
   
+  
     const { id } = useParams();
 
     //  SHOW USERS
   
     useEffect(() => {
-      fetch(BASE_URL + 'users/' + id)
+      fetch(BASE_URL + `users/${id}`)
         .then((res) => res.json())
         .then((json) => setUser(json));
     }, [id]);
@@ -22,6 +23,7 @@ export default function UserDetails() {
     useEffect(() => {
         console.log(user);
     }, [user]);
+
 
     function createBlogPost(blogpostDetails) {
         const newBlogpost = {
@@ -47,15 +49,35 @@ export default function UserDetails() {
 
     
   
-    return (
+      return (
         <div>
         {user && (
           <>
-            <p>First Name: {user.first_name}</p>
-            <p>User Name: {user.user_name}</p>
+            <p>Hi {user.first_name}!</p>
             
-            <div className="card-container  ">
-             
+            <p>We know posting your first blog can be intimidating as all get out!</p>
+               <p> And even if you have already posted your first blog, writers block is always lurking.</p>
+               <h4>Questions?</h4>
+              <p>What do I write about? What should my title be? Who wants to read what I write anyway?</p>
+              <h4>Some helpful Answers:</h4>
+              <ul>
+                <li>Your Blogpost does not have to be a long dissertation!</li>
+                <li>It can be short and sweet with a photo added for context.</li>
+                <li>Write about something you love or you are passionate about. And include a photo for context.</li>
+                <li>Write about something you don't love but you are passionate about. The list of topics are endless.</li>
+                <li>Check out some of the links below on getting started, and then face your fears and write something!</li>
+              </ul>
+              <p>We are looking forward to reading what you have come up with.</p>
+
+              <h3>Getting Started Links</h3>
+                <ul>
+            <li><a href="https://justagirlandherblog.com/what-should-i-blog-about/" style={{color: 'white'}}>I don't know what to Blog about~By Abby Lawson</a></li>
+            <li><a href="https://www.startamomblog.com/what-to-blog-about-first-blog-posts/" style={{color: 'white'}}>What to Blog About When You Don't Know What to Blog About</a></li>
+            </ul>
+
+            
+            <div className="user-container  ">
+            <h4>User Name: {user.user_name}</h4>
             <h3>Add New Blog post</h3>
             <BlogpostForm createBlogpost={createBlogPost} />
             
