4573ab861813c503168f3e41c32e73c807693536
sydney <sydneychase5841@gmail.com>
2021-10-15 19:57:50 -0400

    added Styles for homepage and updated styles for Navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 import { BASE_URL } from '../constraints';
 import UserForm from './User/UserForm';
+import '../components/Styles/Home.css'
 
 
 
@@ -27,24 +28,26 @@ function Home() {
     
 
     return (
-        <div className="box">
+        <div className="container">
         
-        <h1 className="header">Welcome to The Indigo Room Blogs</h1>
-        <div>
+        <h1 className="header">The Indigo Room Blogs</h1>
+        
+        <div className="homebox">
             <h2>It is {new Date().toLocaleTimeString()}.</h2>
 
-        
+        </div>
     
-        <h2>Register to Create Your Account Below</h2>
-            
+        <h2 className="header2">Register to Create Your Account Below</h2>
+        <div className="homebox1"> 
         <p>You don't have to blog to register.</p>
         <p>But you do have to register to blog!</p>
         <p>You can read blogs, post a blog or comment on one.</p>
         <p>Once your user account is created, your last name and email address will no longer be visible on the screen; however, it will be visible to you on your private user profile.</p> 
-        <div className="userForm"></div>
-          </div>
+        </div>
+          <div className="userForm">
+    
           <UserForm createUser={createUser} />
-        
+        </div>
         </div>
 
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,16 @@ import '../components/Styles/Navbar.css'
 
 function Navbar() {
     return (
-        <div className="container-sm">
         <nav className="navbar">
-            <div className="links">
-                <Link to="/">HOME</Link><br/>
-                <Link to="/users">BLOGGERS</Link><br/>
-                <Link to="/blogposts">BLOGS</Link><br/>
+                <Link to="/">HOME</Link>
+                <Link to="/users">BLOGGERS</Link>
+                <Link to="/blogposts">BLOGS</Link>
                 
 
                 
                 
-            </div>
+    
         </nav>
-        </div>
     );
 }
 
